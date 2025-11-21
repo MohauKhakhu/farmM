d647d756ef27c1423be2cb88687575f32729deea
MohauKhakhu <khakhumohau@gmail.com>
2025-11-21 10:22:24 +0200

    m

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+console.log('main.jsx is executing!');
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+console.log('Root element found:', rootElement);
+
+if (rootElement) {
+  rootElement.innerHTML = `
+    <div style="padding: 20px; font-family: Arial; text-align: center;">
+      <h1 style="color: green; font-size: 2rem;">✅ Basic HTML is working!</h1>
+      <p>If you see this, JavaScript is loading and executing.</p>
+      <p>Check the browser console for more info.</p>
+    </div>
+  `;
+} else {
+  console.error('No root element found!');
+  document.body.innerHTML = '<h1 style="color: red;">ERROR: No root element</h1>';
+}
